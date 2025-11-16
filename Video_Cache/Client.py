4ac04c2dfeb576608ec6d1748d1dbfd1d814755c
ReallyEzyGame <lethanhphong010106@gmail.com>
2025-11-16 19:34:56 +0700

    Sửa Lại Chức năng cache kiểu Jitter, thêm các thanh hiển thị thời gian xem, thời lượng của Video, thanh Cache, đồng thời sửa lại cách giao tiếp ở đầu quá trình SETUP nhằm nhận được số khung hình của video. Để sử dụng được chức năng Cache, thay thế toàn bộ các file mã nguồn của bản nguyên mẫu không Cache được bằng các file tương ứng của bản này

--- Video_Cache/Client.py
+++ Video_Cache/Client.py
@@ -1,15 +1,22 @@
-from collections import deque
+import queue
+import time
 from tkinter import *
+from tkinter import ttk
 import tkinter.messagebox
 from PIL import Image, ImageTk
 import socket, threading, sys, traceback, os
 
+
 from RtpPacket import RtpPacket
+
 
 CACHE_FILE_NAME = "cache-"
 CACHE_FILE_EXT = ".jpg"
 
-class Client:
+SLEEP_TIME = 0.04
+
+
+class Client:	
 	INIT = 0
 	READY = 1
 	PLAYING = 2
@@ -20,11 +27,13 @@ class Client:
 	PAUSE = 2
 	TEARDOWN = 3
 
+
 	# Initiation..
 	def __init__(self, master, serveraddr, serverport, rtpport, filename):
 		self.master = master
 		self.master.protocol("WM_DELETE_WINDOW", self.handler)
-		self.createWidgets()
+		
+  
 		self.serverAddr = serveraddr
 		self.serverPort = int(serverport)
 		self.rtpPort = int(rtpport)
@@ -33,9 +42,16 @@ class Client:
 		self.sessionId = 0
 		self.requestSent = -1
 		self.teardownAcked = 0
-		self.connectToServer()
+
 		self.frameNbr = 0
-		self.queue = deque()					# contains frames that is used to cache
+		self.totalFrames = 0
+		self.fps = 20
+		self.frameBuffer = queue.Queue()				# contains frames that is used to cache
+
+		self.playEvent = threading.Event()
+  
+		self.createWidgets()
+		self.connectToServer()
   
 	def createWidgets(self):
 		"""Build GUI."""
@@ -67,9 +83,17 @@ class Client:
 		self.label = Label(self.master, height=19)
 		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5) 
 
-		# Create a time Bar to display time
+		# Create Time Label
+		self.slider_frame = Frame(self.master)
+		self.slider_frame.grid(row=2, column=2, columnspan=4, sticky=W+E, padx=5, pady=5)
+
+		self.time_label = Label(self.slider_frame, text="--:-- / --:--")
+		self.time_label.pack(side=BOTTOM)
+		
+		self.timeline_canvas = Canvas(self.slider_frame, height=20, bg='#333333')
+		self.timeline_canvas.pack(side=BOTTOM, fill=X, expand=True)
+	
   
- 
 	def setupMovie(self):
 		"""Setup button handler."""
 		if self.state == self.INIT:
@@ -90,39 +114,84 @@ class Client:
 		"""Pause button handler."""
 		if self.state == self.PLAYING:
 			self.sendRtspRequest(self.PAUSE)
+			self.playEvent.set()
 	
  
 	def playMovie(self):
 		"""Play button handler."""
 		if self.state == self.READY:
+			self.playEvent.clear()
+
+			#while not self.frameBuffer.empty():
+			#	try:
+			#		self.frameBuffer.get_nowait()
+			#	except queue.Empty:
+			#		break
+   
 			# Create a new thread to listen for RTP packets
 			threading.Thread(target=self.listenRtp).start()
-			self.playEvent = threading.Event()
+			threading.Thread(target=self.playVideoThread).start()
+   
+			# self.playEvent = threading.Event()
 			self.playEvent.clear()
 			self.sendRtspRequest(self.PLAY)
-	
  
+ 
+	def updateTimeLabel(self, currentFrame):
+		"""Update the Time Label"""
+		if self.totalFrames == 0:
+			return
+
+		current_sec = currentFrame / self.fps
+		total_sec = self.totalFrames / self.fps
+
+		current_time_str = "{:02}:{:02}".format(int(current_sec // 60), int(current_sec % 60))
+  
+		total_time_str = "{:02}:{:02}".format(int(current_sec // 60), int(total_sec % 60))
+
+		self.time_label.config(text=f"{current_time_str} / {total_time_str}")
+  
+  
+	def updateTimelineCanvas(self):
+		"""ReDraw the canvas time bar(cache, watched)"""
+		canvas_width = self.timeline_canvas.winfo_width()
+		canvas_height = self.timeline_canvas.winfo_height()
+  
+		# Stop if Canvas has not been draw yet
+		if canvas_width <= 1 or canvas_height <= 1 or self.totalFrames == 0:
+			return
+		currentFrame = self.frameNbr
+
+		cachedFrames = min(self.totalFrames, currentFrame + self.frameBuffer.qsize())
+  
+		# Calculate Pixel position
+		watched_x = (currentFrame / self.totalFrames) * canvas_width
+		cached_x = (cachedFrames / self.totalFrames) * canvas_width
+  
+		# Draw layer by layer
+		bar_y_start = 0
+		bar_y_end = 100
+		# Bar's  Theme
+		self.timeline_canvas.create_rectangle(0,  bar_y_start, canvas_width, bar_y_end, fill="#555555", width=0)
+		# Cache Bar
+		self.timeline_canvas.create_rectangle(0, bar_y_start, cached_x, bar_y_end, fill="#AAAAAA", width=0)
+		# Watched Bar
+		self.timeline_canvas.create_rectangle(0,  bar_y_start, watched_x, bar_y_end, fill="#FF0000", width=0)
+		# Thumb Button
+		thumb_radius = 12
+		thumb_y_center = canvas_height / 2
+  
+		self.timeline_canvas.create_oval(watched_x - thumb_radius, thumb_y_center - thumb_radius, watched_x + thumb_radius, thumb_y_center + thumb_radius, fill="#FF0000", outline='#FFFFFF')
+  
+  
 	def listenRtp(self):		
 		"""Listen for RTP packets."""
 		while True:
 			try:
 				data = self.rtpSocket.recv(20480)
 				if data:
-					rtpPacket = RtpPacket()
-					rtpPacket.decode(data)
-					
-					currFrameNbr = rtpPacket.seqNum()
-					print("Current Seq Num: " + str(currFrameNbr))
-					#
-					self.queue.append(rtpPacket)
-					#
-					if len(self.queue) >= 20:
-					#if currFrameNbr > self.frameNbr: # Discard the late packet
-						packet = self.queue.popleft()
-						self.frameNbr = packet.seqNum()
-      
-						self.updateMovie(self.writeFrame(packet.getPayload()))
-						#self.updateMovie(self.writeFrame(rtpPacket.getPayload()))
+					self.frameBuffer.put(data)
+     
 			except:
 				# Stop listening upon requesting PAUSE or TEARDOWN
 				if self.playEvent.is_set(): 
@@ -135,7 +204,41 @@ class Client:
 					self.rtpSocket.close()
 					break
 		
-  			
+	def playVideoThread(self):
+		while True:
+			try:
+				# Check for PAUSE or TEARDOWN
+				if self.playEvent.is_set():
+					break
+				
+				data = self.frameBuffer.get(timeout=0.5)
+
+				if not data:
+					continue
+				rtpPacket = RtpPacket()
+				rtpPacket.decode(data)
+
+				currFrameNbr = rtpPacket.seqNum()
+				print("Current Seq Num: " + str(currFrameNbr))
+
+				# Discard the late Packet
+				if currFrameNbr > self.frameNbr:
+					self.frameNbr = currFrameNbr
+					if self.frameNbr % 6 == 0:
+						self.updateTimelineCanvas()
+						self.updateTimeLabel(currFrameNbr)
+	
+					self.updateMovie(self.writeFrame(rtpPacket.getPayload()))
+				# Handle Jitter
+				time.sleep(SLEEP_TIME)
+    
+			except queue.Empty:
+				print("Jitter Buffer is Empty, Waitting for data...")
+				# Escape when pause while waiting
+				if self.playEvent.is_set():
+					break
+  
+  
 	def writeFrame(self, data):
 		"""Write the received frame to a temp image file. Return the image file."""
 		cachename = CACHE_FILE_NAME + str(self.sessionId) + CACHE_FILE_EXT
@@ -145,7 +248,7 @@ class Client:
 		
 		return cachename
 	
- 
+
 	def updateMovie(self, imageFile):
 		"""Update the image file as video frame in the GUI."""
 		photo = ImageTk.PhotoImage(Image.open(imageFile))
@@ -241,6 +344,15 @@ class Client:
 					if self.requestSent == self.SETUP:
 						# Update RTSP state.
 						self.state = self.READY
+
+						for line in lines:
+							if line.startswith('Video-Length:'):
+								self.totalFrames = int(line.split(' ')[1])
+								print(f"Server reports {self.totalFrames} frames")
+						
+						if self.totalFrames > 0:
+							pass
+     
 						# Open RTP port.
 						self.openRtpPort() 
 					elif self.requestSent == self.PLAY:
@@ -248,7 +360,7 @@ class Client:
 					elif self.requestSent == self.PAUSE:
 						self.state = self.READY
 						# The play thread exits. A new thread is created on resume.
-						self.playEvent.set()
+						# self.playEvent.set()
 					elif self.requestSent == self.TEARDOWN:
 						self.state = self.INIT
 						# Flag the teardownAcked to close the socket.
@@ -269,6 +381,7 @@ class Client:
 			self.rtpSocket.bind(('', self.rtpPort))
 		except:
 			tkinter.messagebox.showwarning('Unable to Bind', 'Unable to bind PORT=%d' %self.rtpPort)
+
 
 	def handler(self):
 		"""Handler on explicitly closing the GUI window."""
