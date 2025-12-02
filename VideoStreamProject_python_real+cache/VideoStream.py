5d80ff2ed54dfc2f12dbfb1aa7d864ebafd48147
2wr <taolaolgc@gmail.com>
2025-12-02 17:19:44 +0700

    correct mjpeg handling

--- VideoStreamProject_python_real+cache/VideoStream.py
+++ VideoStreamProject_python_real+cache/VideoStream.py
@@ -1,3 +1,5 @@
+import os
+
 class VideoStream:
 	def __init__(self, filename):
 		self.filename = filename
@@ -9,14 +11,35 @@ class VideoStream:
 		
 	def nextFrame(self):
 		"""Get next frame."""
-		data = self.file.read(5) # Get the framelength from the first 5 bits
-		if data: 
-			framelength = int(data)
-							
-			# Read the current frame
-			data = self.file.read(framelength)
-			self.frameNum += 1
-		return data
+		data = bytearray()
+
+		prev = None
+		while True:
+			byte = self.file.read(1)
+			if not byte:
+				break
+			if prev == b'\xFF' and byte == b'\xD8':
+				data.extend(b"\xFF\xD8")
+				break
+			prev = byte
+		
+		chunk = self.file.read(4096)
+		prev = None
+		while chunk:
+			break_f = False
+			for i in range(len(chunk)):
+				if prev == 0xFF and chunk[i] == 0xD9:
+					break_f = True
+					data.extend(chunk[:i + 1])
+					self.file.seek(-(len(chunk) - 1 - i), os.SEEK_CUR)
+					break
+				prev = chunk[i]
+			if break_f:
+				break
+			data.extend(chunk)
+			chunk = self.file.read(4096)
+		self.frameNum += 1
+		return bytes(data)
 		
 	def frameNbr(self):
 		"""Get frame number."""
